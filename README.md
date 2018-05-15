5b76f1242ef9a130ad0195f29e95d389474d405a
Julien Noble <jnoble@airintservices.com>
2018-05-15 13:09:13 +0200

    added bulk inserts, global conditions, custom log functions and updated README

--- README.md
+++ README.md
@@ -1,36 +1,52 @@
 # sequelize-data-transfer
-[![Build Status](https://travis-ci.org/zxdong262/sequelize-data-transfer.svg?branch=master)](https://travis-ci.org/zxdong262/sequelize-data-transfer)
 
-- transfer `sequelize` db data from one db to another db, with different data model.
-- designed only for `sequelize.import` user.
+Based on
+[zxdong262's original repo](https://github.com/zxdong262/sequelize-data-transfer),
+adds bulk and global conditional cloning.
+
+This was motivated by the fact we use this package internally to fast clone huge
+dbs for testing, development and utilities. In terms of data volume, we're
+talking the equivalent of 300MB+ flat sql dumps. The original package would
+insert records one at a time, so as soon as you would have more than 2000
+entries, the operation would take ages. Hence the need to make bulk inserts and
+filter the data globally if there are some global foreign patterns, like cloning
+data only for a specific customer for example.
+
+The key points are :
+
+* transfer `sequelize` db data from one db to another db, with different data
+  model.
+* designed only for `sequelize.import` user.
+* clone all or part of your data
+* clone fast
 
 ## use
+
 ```javascript
+const { transfer } = require('sequelize-data-transfer');
+let path = require('path');
 
-const transfer = require('sequelize-data-transfer').transfer
-let path = require('path')
-
-//old data define
+// old data define
 let oldDb = {
-    dialect: 'postgres',
-    host: 'localhost',
-    database: 'dbname1',
-    username: 'username1',
-    password: 'password1',
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000,
-    },
-    port: 5432,
-    define: {
-      charset: 'utf8',
-      timestamps: false
-    },
-    modelsFolder: path.resolve(__dirname, './old-db-models')
-  }
-  
-//new data define
+  dialect: 'postgres',
+  host: 'localhost',
+  database: 'dbname1',
+  username: 'username1',
+  password: 'password1',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+  port: 5432,
+  define: {
+    charset: 'utf8',
+    timestamps: false,
+  },
+  modelsFolder: path.resolve(__dirname, './old-db-models'),
+};
+
+// new data define
 let newDb = {
   dialect: 'postgres',
   host: 'localhost',
@@ -45,46 +61,106 @@ let newDb = {
   port: 5432,
   define: {
     charset: 'utf8',
-    timestamps: false
+    timestamps: false,
   },
-  modelsFolder: path.resolve(__dirname, './new-db-models')
-}
+  modelsFolder: path.resolve(__dirname, './new-db-models'),
+};
 
-//config
+// config
 let transferConfig = {
-  array: [{
-    from: 'OldUser', //old db Model Name
-    to: 'User', //new db Model Name
-    transfer: old => {
-      return {
-        id: old.id,
-        name: old.user_name
-      }
-    }
-  }]
-}
+  clone: true, // if true, clones the entire db, else uses the array property
+  array: [
+    {
+      from: 'OldUser', //old db Model Name
+      to: 'User', //new db Model Name
+      transfer: old => {
+        return {
+          id: old.id,
+          name: old.user_name,
+        };
+      },
+    },
+  ],
+  // sequelize options passed when getting rows from old db
+  // you can pass any sequelize option, include, where
+  // if raw is true, the inserts will be in bulk
+  // if false, the inserts will be one row at a time
+  queryOptions: { raw: true },
+};
 
-//now do the transfer
-transfer({oldDb, newDb, transferConfig})
-.then(() => {
-  console.log('done')
-})
-.catch(e => {
-  console.log(e.stack)
-})
+// global conditions applied to getting rows from old db if applicable
+// each array represents one column and the search pattern
+// the pattern respects Sequelize's operators, so you can use Op.*
+// the query is built based on what columns the current model has
+// if a model does not have a column, the condition is ignored
+// if only one column is present, only this one will be added to
+// model condition
+let where = [{ userId: 42 }, { customerId: 13 }];
 
-//complete clone (should be same data models)
-transfer({oldDb, newDb, { clone: true }})
-.then(() => {
-  console.log('done')
-})
-.catch(e => {
-  console.log(e.stack)
-})
+// if queryOptions.raw is true, transfer will insert in bulk
+// by default, bulk inserts are divided in bulks of 10000 rows at a time
+// to prevent max_allowed_packet exceptions, you can limit globally or
+// specifically how much rows go into a bulk division
+let bulkLimit = {
+  default: 5000, // applies to all models and replaces default 10000
+  User: 10, // applies only to model named User
+};
+
+// you can add custom log and error functions
+// if ommited, will default to console.debug
+// and console.error
+let logFn = (...args) => {
+  console.log('Transfer:', ...args);
+};
+let errorFn = (...args) => {
+  console.error('ERROR:', ...args);
+};
+
+// now do the transfer
+transfer({ oldDb, newDb, transferConfig, where, bulkLimit, logFn, errorFn })
+  .then(() => {
+    console.log('done');
+  })
+  .catch(e => {
+    console.log(e.stack);
+  });
 
 //done
 ```
+
+## Misc
+
+The exported API was built to be used with `async/await` and ES6 generators. It
+requires a nodejs version which supports native promises or you need to check
+out projects like [bluebird](http://bluebirdjs.com/docs/getting-started.html) to
+add support.
+
+You can import `initDb` to quickly get a sequelize instance to use, for instance
+to execute side operations on db before cloning it. We use it to prepare our
+clone db on test environments, which includes dropping the data, cloning it with
+conditions, and returning a sequelize instance to use in our tests.
+
+```javascript
+const { initDb, transfer } = require('sequelize-data-transfer');
+
+const testDb= { /* data define */ }
+const testDbInstance = await initDb(testDb);
+await testDbInstance.drop();
+
+// setup your transfer
+await transfer({ /* transfer options */ });
+```
+
+## Changelog
+
+### 16/05/2018
+
+* added bulk insert and query options
+* added global conditions
+* added custom log and error functions
+* added externally callable `initDb`
+* prettified base code with semicolons and comma rules
+
 ## License
+
 MIT
-  
-  
--- index.js
+++ index.js
@@ -1,38 +1,33 @@
-
-const Sequelize = require('sequelize')
-const co = require('co')
-const fs = require('fs')
-const path = require('path')
+const Sequelize = require('sequelize');
+const co = require('co');
+const fs = require('fs');
+const path = require('path');
 
 function* initDb(model) {
-
-  let { modelsFolder } = model
-  let db = {}
+  let { modelsFolder } = model;
+  let db = {};
 
   const sequelize = new Sequelize(
     model.database,
     model.username,
     model.password,
-    model
-  )
-
-  yield sequelize.authenticate()
-  yield sequelize.sync()
+    model,
+  );
 
   fs
     .readdirSync(modelsFolder)
     .filter(file => {
-      return (file.indexOf('.') !== 0)
+      return file.indexOf('.') !== 0;
     })
     .forEach(file => {
-      const mod = sequelize.import(path.join(modelsFolder, file))
-      db[mod.name] = mod
-    })
+      const mod = sequelize.import(path.join(modelsFolder, file));
+      db[mod.name] = mod;
+    });
 
-  sequelize.db = db
-  yield sequelize.authenticate()
-  yield sequelize.sync()
-  return sequelize
+  sequelize.db = db;
+  yield sequelize.authenticate();
+  yield sequelize.sync();
+  return sequelize;
 }
 
 function mkTransferArray(sql) {
@@ -40,47 +35,126 @@ function mkTransferArray(sql) {
     return {
       from: model,
       to: model,
-      transfer: old => old
-    }
-  })
+      transfer: old => old,
+    };
+  });
 }
 
-function* work({oldDb, newDb, transferConfig}) {
-  let t1 = new Date().getTime()
-  let sq1 = yield initDb(oldDb)
-  let sq2 = yield initDb(newDb)
-  let isClone = !!transferConfig.clone
-  let array = isClone ? mkTransferArray(sq2) : transferConfig.array
-  for(let i = 0, len = array.length;i < len;i ++) {
+// sequelize errors
+function logSequelizeErrors(err, logError) {
+  if (err.errors)
+    err.errors.map(error =>
+      logError('===> "', error.path, '"', error.type, 'error :', error.message),
+    );
+}
 
-    let conf = array[i]
-    let {from, to, transfer} = conf
-    let rows = yield sq1.db[from].findAll()
-    console.log('start:trasnfer old db model:', from, 'to', 'new db model:', to)
-    console.log('old db', from, 'has', rows.length, 'data')
+function* work(options) {
+  let {
+    oldDb,
+    newDb,
+    transferConfig,
+    where,
+    bulkLimit,
+    logFn,
+    errorFn,
+  } = options;
+  let log = logFn || console.debug;
+  let logError = errorFn || console.error;
 
-    for(let j = 0, len1 = rows.length;j < len1;j ++) {
-      let item = rows[j].get({ plain: true })
-      let tar = transfer(item)
-      yield sq2.db[to].create(tar)
+  let t1 = new Date().getTime();
+  let sq1 = yield initDb(oldDb);
+  let sq2 = yield initDb(newDb);
+  let isClone = !!transferConfig.clone;
+  let array = isClone ? mkTransferArray(sq2) : transferConfig.array;
+
+  for (let i = 0, len = array.length; i < len; i++) {
+    let conf = array[i];
+    let { from, to, transfer } = conf;
+
+    try {
+      let rows;
+      let formattedWhere = where
+        ? where.reduce((query, opt) => {
+            let column = Object.keys(opt)[0];
+            if (sq1.db[from].rawAttributes[column])
+              return { ...query, [column]: opt[column] };
+            return query;
+          }, {})
+        : null;
+      rows = yield sq1.db[from].findAll({
+        ...transferConfig.queryOptions,
+        where: formattedWhere,
+      });
+
+      log('start: transfer old db model:', from, 'to new db model:', to);
+      if (formattedWhere && Object.keys(formattedWhere).length > 0)
+        log('filtered model:', from, 'with', JSON.stringify(formattedWhere));
+      log('old db', from, 'has', rows.length, 'data');
+
+      yield sq2.transaction(transaction => {
+        let promise = Promise.resolve();
+        if (transferConfig.queryOptions.raw) {
+          // by default 10000 records at a time to prevent max_allowed_packet exceptions
+          // configurable for each table or for all
+          let limit = bulkLimit[to] || bulkLimit.default || 10000;
+          for (let j = 0; j < rows.length; j += limit) {
+            promise = promise
+              .then(() => {
+                const limitedRows = rows.slice(j, j + limit);
+                return sq2.db[to].bulkCreate(limitedRows, {
+                  transaction,
+                });
+              })
+              .catch(err => {
+                logError(
+                  'failed adding bulk items',
+                  j,
+                  'to',
+                  j + limit,
+                  'to model:',
+                  to,
+                  '->',
+                  err.message,
+                );
+                logSequelizeErrors(err, logError);
+              });
+          }
+        } else {
+          for (let j = 0, len1 = rows.length; j < len1; j++) {
+            let item = rows[j].get({ plain: true });
+            let tar = transfer(item);
+            promise = promise
+              .then(() => sq2.db[to].create(tar, { transaction }))
+              .catch(err => {
+                logError(
+                  'failed adding item',
+                  j,
+                  'to model:',
+                  to,
+                  '->',
+                  err.message,
+                );
+                logSequelizeErrors(err, logError);
+              });
+          }
+        }
+        return promise;
+      });
+      log('done: transfer old db model:', from, 'to new db model:', to);
+    } catch (err) {
+      logError('failed copying model:', to, '->', err.message);
     }
-
-    console.log('done:trasnfer old db model:', from, 'to', 'new db model:', to)
   }
 
+  let t2 = new Date().getTime();
+  log('all done :');
+  log('cost time :', (t2 - t1) / 1000, 's');
 
-
-  let t2 = new Date().getTime()
-  console.log('all done:')
-  console.log('cost time:', (t2 - t1) / 1000, 's')
-
-  return Promise.resolve()
+  return Promise.resolve();
 }
 
-exports.initDb = initDb
+exports.initDb = co.wrap(initDb);
 
-exports.transfer = ({oldDb, newDb, transferConfig}) => {
-  
-  return co(work({oldDb, newDb, transferConfig}))
-
-}
+exports.transfer = options => {
+  return co(work(options));
+};
